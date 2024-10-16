6354dedc1e8a98c1c0fdd0382b53e01b7263d7ed
abdullah-gashia <s6630613012@phuket.psu.ac.th>
2024-10-16 21:05:28 +0700

    first commit

---
+++ module.js
@@ -0,0 +1,17 @@
+export function add(a, b) {
+    return a + b;
+} 
+
+export function greet() {
+    return "Hello World";
+}
+
+export const name = "John doe";
+
+export const person = {
+    firstName : "Jack",
+    LastName : "Son",
+    age : 20
+}
+
+export const arr = [10 , 20 , 30 , 40];
